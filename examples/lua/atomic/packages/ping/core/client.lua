5c2de1ff84dfa24ae4dc0e9dbb418efe1809fa53
smokingplaya <smokingplaya@yandex.ru>
2025-11-20 07:02:18 +0300

    refactor(examples): change to aliases

--- examples/lua/atomic/packages/ping/core/client.lua
+++ examples/lua/atomic/packages/ping/core/client.lua
@@ -1,11 +1,11 @@
 ---@class ExamplePackagePing: Atomic.Package
-local package = atomic.package.current()
+local package = current()
 
 --- this function will send
 --- an asynchronous network message to the server
 --- and wait for a response
 function package:ping()
-  local instant = atomic.time.newInstant()
+  local instant = Instant()
 
   coroutine.start(function()
     local message = self:sendNetworkMessageAsync("Ping", {})
@@ -13,14 +13,12 @@ function package:ping()
     local ms = instant:elapsed():as_millis()
 
     package.logger:debug("Server ping took %sms", ms)
-    package.logger:debug("Server response: %s", message.contentz)
+    package.logger:debug("Server response: %s", message.content)
   end)
 end
 
 --- Removing console command
 --- when package will disabled
----
---- !!! it's optional
 package:listen("onDisable", function()
   concommand.Remove("do_ping")
 end)
