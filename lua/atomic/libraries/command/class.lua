8c8da5785b1d78da36b21facc8435359eb1ac033
smokingplaya <smokingplaya@yandex.ru>
2025-12-31 23:29:24 +0300

    feat(command): make argument can be optional, fix CAMI integration

--- lua/atomic/libraries/command/class.lua
+++ lua/atomic/libraries/command/class.lua
@@ -1,13 +1,15 @@
 ---@alias Atomic.Command.ArgumentKind "number" | "string" | "boolean" | "time" | "player"
----@alias Atomic.Command.ExecuteFunc function(executor: Player, arguments: table<string, Atomic.Command.ArgumentKind): string?
+---@alias Atomic.Command.ExecuteFunc fun(executor: Player, arguments: table<string, Atomic.Command.ArgumentKind>): string?
+---@alias Atomic.Command.Argument { name: string, kind: Atomic.Command.ArgumentKind, isOptional: boolean }
 
 ---@class Atomic.Command: Atomic.Class
 ---@field private _name string
 ---@field private _permission? string
 ---@field private _cooldown? integer
----@field private _arguments {[1]: string, [2]: Atomic.Command.ArgumentKind}[]
+---@field private _arguments Atomic.Command.Argument[]
 ---@field private _execute Atomic.Command.ExecuteFunc
 ---@field private _enabled boolean Internal
+---@field private _hasOptional boolean Internal
 local Command = atomic.class.create("Command")
 atomic.class.register(Command, atomic.class.pseudo)
 
@@ -20,6 +22,7 @@ function Command:init(name, permission, cooldown)
 	self._cooldown = cooldown
   self._arguments = {}
   self._enabled = true
+	self._hasOptional = false
 end
 
 ---@return integer?
@@ -27,11 +30,29 @@ function Command:getCooldown()
 	return self._cooldown
 end
 
+---@return string
+function Command:getName()
+	return self._name
+end
+
+---@return Atomic.Command.Argument[]
+function Command:getArguments()
+	return self._arguments
+end
+
 ---@param name string
 ---@param kind Atomic.Command.ArgumentKind
 ---@param isOptional? boolean = false
 ---@return self
 function Command:argument(name, kind, isOptional)
+	if (not self._hasOptional and isOptional) then
+		self._hasOptional = true
+	end
+
+	if (self._hasOptional and not isOptional) then
+		error("in an argument chain a required argument cannot be after an optional argument")
+	end
+
 	self._arguments[#self._arguments + 1] = { name = name, kind = kind, isOptional = isOptional or false }
 
 	return self
@@ -49,20 +70,32 @@ end
 ---@param player Player
 ---@param permission string
 local function hasRightToExecute(player, permission)
-	if (not permission) then
+	if (not permission or not IsValid(player)) then
 		return true
 	end
 
-	if (IsValid(player)) then
-		---@diagnostic disable-next-line
-		CAMI.PlayerHasAccess(player, permission, function(allow)
-			coroutine.resume(allow)
-		end)
+	---@diagnostic disable
 
-		return coroutine.yield()
-	else -- console
-		return true
+	local co = coroutine.running()
+	local result
+	local isYielded = false
+
+	CAMI.PlayerHasAccess(player, permission, function(hasAccess)
+		if (not result) then
+			result = hasAccess
+			if (isYielded) then
+				coroutine.resume(co, hasAccess)
+			end
+		end
+	end)
+
+	if (not result) then
+		isYielded = true
+		result = coroutine.yield()
+		isYielded = false
 	end
+
+	return result
 end
 
 ---@param executor Player
