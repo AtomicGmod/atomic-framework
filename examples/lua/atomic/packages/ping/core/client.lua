485c5811d0aee97dff0c6e46e6eab5b225c79592
smokingplaya <smokingplaya@yandex.ru>
2026-01-27 23:52:04 +0300

    update examples

--- examples/lua/atomic/packages/ping/core/client.lua
+++ examples/lua/atomic/packages/ping/core/client.lua
@@ -1,10 +1,12 @@
 ---@class ExamplePackagePing: Atomic.Package
 local package = current()
 
+-- Asynchronous variant
+
 --- this function will send
 --- an asynchronous network message to the server
 --- and wait for a response
-function package:ping()
+function package:pingAsync()
   local instant = Instant()
 
   async(function()
@@ -12,17 +14,42 @@ function package:ping()
 
     local ms = instant:elapsed():as_millis()
 
-    package.logger:debug("Server ping took %sms", ms)
-    package.logger:debug("Server response: %s", message.content)
+    self.logger:debug("Server ping took %sms", ms)
+    self.logger:debug("Server response: %s", message.content)
   end)
 end
 
---- Removing console command
---- when package will disabled
-package:listen(function()
-  concommand.Remove("do_ping")
-end, "onDisable")
+concommand.Add("ping_async", function()
+	package:pingAsync()
+end)
 
-concommand.Add("do_ping", function()
-	package:ping()
+-- Synchronous variant
+
+---@type table<string, Atomic.Time.Instant>
+package.syncMessagesId = {}
+
+function package:ping()
+  local instant = Instant()
+
+  -- every network message have id
+  local messageId = self:sendNetworkMessage("Ping", {})
+
+  -- saving network packet sending time
+  self.syncMessagesId[messageId] = instant
+end
+
+package:onNetworkMessage(function(self, message)
+  local response = message.content
+
+  local time = self.syncMessagesId[message:getId()]
+
+  if (time) then
+    self.logger:debug("Server ping took %sms", time:elapsed():as_millis())
+  end
+
+  self.logger:debug("Server response: %s", response)
+end, "Ping")
+
+concommand.Add("ping", function()
+  package:ping()
 end)
