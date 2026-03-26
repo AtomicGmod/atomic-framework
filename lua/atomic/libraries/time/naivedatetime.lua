de1b0f793f5c7c3f8ab046c1529949b041da1c48
smokingplaya <smokingplaya@yandex.ru>
2026-03-26 03:21:31 +0300

    feat(time): add wip NaiveDateTime

---
+++ lua/atomic/libraries/time/naivedatetime.lua
@@ -0,0 +1,93 @@
+-- A class for contain timestamp,
+-- close to the Rust (chrono::NaiveDateTime) implementation
+
+local class = atomic.class
+
+---@class Atomic.Time.NaiveDateTime: Atomic.Class
+---@field private year integer
+---@field private month integer
+---@field private day integer
+---@field private hour integer
+---@field private minute integer
+---@field private second integer
+local NaiveDateTime = class.create("NaiveDateTime")
+class.register(NaiveDateTime, atomic.class.pseudo)
+
+---@param year integer
+---@param month integer
+---@param day integer
+---@param hour integer
+---@param minute integer
+---@param second integer
+function NaiveDateTime:init(year, month, day, hour, minute, second)
+  self.year = year
+  self.month = month
+  self.day = day
+  self.hour = hour
+  self.minute = minute
+  self.second = second
+end
+
+---@private
+function NaiveDateTime:__tostring()
+  -- iso 8601
+  return "NaiveDateTime [" .. tostring(self:format("%Y-%m-%dT%H:%M:%S")) .. "]"
+end
+
+--- Returns UNIX Timestamp without current timezone
+---@return number
+function NaiveDateTime:getTimestamp()
+  return os.time({
+    year = self.year,
+    month = self.month,
+    day = self.day,
+    hour = self.hour,
+    min = self.minute,
+    sec = self.second
+  })
+end
+
+---@see https://wiki.facepunch.com/gmod/os.date
+---@return string
+function NaiveDateTime:format(formatStr)
+  ---@type string
+  return os.date(formatStr, self:getTimestamp())
+end
+
+--- Formats the time string as ISO 8601
+---
+--- Output example `2026-03-12T14:25:30`
+---@return string
+function NaiveDateTime:getIso8601()
+  return self:format("%Y-%m-%dT%H:%M:%S")
+end
+
+---@return integer
+function NaiveDateTime:getYear()
+  return self.year
+end
+
+---@return integer
+function NaiveDateTime:getMonth()
+  return self.month
+end
+
+---@return integer
+function NaiveDateTime:getDay()
+  return self.day
+end
+
+---@return integer
+function NaiveDateTime:getHour()
+  return self.hour
+end
+
+---@return integer
+function NaiveDateTime:getMinute()
+  return self.minute
+end
+
+---@return integer
+function NaiveDateTime:getSecond()
+  return self.second
+end
